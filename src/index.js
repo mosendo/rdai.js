5a1ab33ed1cc32b85a99d825e2bc9769425b6387
Nour Haridy <nourharidy@yahoo.com>
2020-03-04 06:25:26 +0200

    initial release

--- src/index.js
+++ src/index.js
@@ -1,13 +1,87 @@
-import "./index.css";
+import { DAI_ADDRESS, RDAI_ADDRESS } from "../config";
+import Web3 from "web3";
+import axios from "axios";
+import RDAI_ABI from "../abi/rdai";
+import DAI_ABI from "../abi/dai";
 
-class MyLibrary {
-  constructor() {
-    console.log("Library constructor loaded");
+const cTokenApi = "https://api.compound.finance/api/v2/ctoken";
+
+class RedeemDai {
+  web3;
+  rdaiContract;
+  daiContract;
+  DAI_ADDRESS;
+  RDAI_ADDRESS;
+
+  constructor(web3, options) {
+    if (typeof web3 === "undefined") throw new Error("Web3 must be provided");
+    if (typeof web3.givenProvider === "undefined")
+      throw new Error("Web3 instance does not support .givenProvider property");
+    this.web3 = new Web3(web3.givenProvider);
+    this.DAI_ADDRESS = options.DAI_ADDRESS || DAI_ADDRESS;
+    this.RDAI_ADDRESS = options.RDAI_ADDRESS || RDAI_ADDRESS;
+    this.daiContract = new web3.eth.Contract(DAI_ABI, this.DAI_ADDRESS);
+    this.rdaiContract = new web3.eth.Contract(RDAI_ABI, this.RDAI_ADDRESS);
   }
 
-  myMethod = () => {
-    console.log("Library method fired");
+  formatAmount = amount => {
+    if (typeof amount === "number")
+      return this.web3.utils.toWei(amount, "ether");
+    else if (typeof amount === "string") return new this.web3.utils.BN(amount);
+    else if (typeof amount === "object") return amount;
+    else throw new Error("unsupported amount type");
+  };
+
+  approve = () => {
+    const BN = this.web3.utils.BN;
+    const MAX_UINT = new BN(2).pow(256).sub(1);
+    return this.daiContract.methods.approve(this.RDAI_ADDRESS, MAX_UINT).send();
+  };
+
+  mintWithHat = (amount, hat) => {
+    amount = this.formatAmount(amount);
+    return this.rdaiContract.methods
+      .mintWithNewHat(amount, hat.recipients, hat.proportions)
+      .send();
+  };
+
+  mint = amount => {
+    amount = this.formatAmount(amount);
+    return this.rdaiContract.methods.mintamount(amount).send();
+  };
+
+  getHat = address => {
+    return this.rdaiContract.methods.getHatByAddress(address).call();
+  };
+
+  redeem = amount => {
+    if (typeof amount != "undefined") {
+      amount = this.formatAmount(amount);
+      return this.rdaiContract.methods.redeem(amount).send();
+    } else {
+      return this.rdaiContract.methods.redeemAll().send();
+    }
+  };
+
+  getDaiBalance = address => {
+    return this.daiContract.methods.balanceOf(address).call();
+  };
+
+  getRdaiBalance = address => {
+    return this.rdaiContract.methods.balanceOf(address).call();
+  };
+
+  getInterestEarned = address => {
+    return this.rdaiContract.methods.interestPayableOf(address).call();
+  };
+
+  getInterestRate = async () => {
+    const cTokens = (await axios.get(cTokenApi)).data.cToken;
+    const cDai = cTokens.filter(cToken => cToken.symbol === "cDAI")[0];
+    const rate = cDai.supply_rate.value;
+    const percentage = (rate * 100).toFixed(2);
+    return Number(percentage);
   };
 }
 
-export default MyLibrary;
+export default RedeemDai;
